0e96aa9a72a1f41d5df031076c1ace665c3472fe
iyegoroff <iegoroff@gmail.com>
2022-07-24 02:53:43 +0300

    m

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  coverageDirectory: 'coverage',
-  collectCoverage: true,
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['js', 'ts', 'tsx', 'jsx'],
-  coveragePathIgnorePatterns: ['/node_modules/', 'test/'],
-  testRegex: '\\.spec\\.tsx?$'
-}
